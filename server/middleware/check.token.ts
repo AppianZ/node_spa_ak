46276f6f497b883275cb39a8a005246d35307fc9
Appian <zhang4baichi@sina.com>
2017-11-07 19:27:16 +0800

    new qs axios9

--- server/middleware/check.token.ts
+++ server/middleware/check.token.ts
@@ -2,18 +2,14 @@ import { Request, Response, NextFunction } from "@types/express";
 import * as http from '../libs/axios';
 const expiresTime:number = 3 * 24 * 60 * 60 * 1000; // cookies 过期时间 3d
 import * as util from '../libs/util';
+import TestApi from '../apis/test';
 
 export default async function (req:Request, res:Response, next:NextFunction) {
   console.log('------ checktoken22222 ---- data ----');
   console.log(req.body);
 
   try {
-      const ret = await http.post(this.req, '/api/v1/uac/oauth/token', {
-          data: req.body,
-          headers: {
-              'Authorization': 'Basic c29wX2FwcF9wbGF0Zm9ybTpZWEJ3Y0d4aGRHWnZjbTFmYzJWamNtVjA=',
-          },
-      }, 'form');
+      const ret = await new TestApi(req).getToken(req.body);
 
       // res设置cookie.x-auth-token
       const token = util.UpperFirstLetter(ret.data.token_type) + ' ' + ret.data.access_token;
