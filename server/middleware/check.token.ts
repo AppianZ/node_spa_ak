8b318a3f75faad5bef1c77d34014a8054f0cab37
Appian <zhang4baichi@sina.com>
2017-11-07 19:55:11 +0800

    axios 携带存好的cookie4

--- server/middleware/check.token.ts
+++ server/middleware/check.token.ts
@@ -9,7 +9,7 @@ export default async function (req:Request, res:Response, next:NextFunction) {
 
   if (req.cookies['authorization']) {
       console.log('------ cookies里的token尚未过期 ----');
-      req['Authorization'] = req.cookies['authorization'];
+      req.headers['Authorization'] = req.cookies['authorization'];
       next();
   } else {
       try {
