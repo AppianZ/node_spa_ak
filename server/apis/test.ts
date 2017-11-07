cff0778e9c632f59dab3e01145e53173dd6eb78b
Appian <zhang4baichi@sina.com>
2017-11-07 16:42:31 +0800

    test auth token post http12

--- server/apis/test.ts
+++ server/apis/test.ts
@@ -31,7 +31,7 @@ class TestApi {
     }
 
     async getToken(data:any) {
-        const token = await http.get(this.req, '/content/customer/content', {
+        const token = await http.get(this.req, '/content/customer/content?platform=app', {
             headers: {
                 'Authorization': 'Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiNTllODNlN2NkZDdjNGE1MWI3MDM2ZmFjIiwidXNlcl9uYW1lIjoiNTllODNlN2NkZDdjNGE1MWI3MDM2ZmFjIiwic3lzdGVtX2lkIjoiYXBwX3BsYXRmb3JtIiwiZXh0cmEiOnt9LCJzY29wZSI6WyJvcGVuaWQiLCJ3cml0ZSIsInJlYWQiLCJzb3BfYmFzZSIsInNvcF91YWMiLCJzb3Atc29uaWMtc2VydmljZSIsInNvcC1jdXN0LXNlcnZpY2UiLCJzb3AtY3VzdC1wbGF0Zm9ybS1zZXJ2aWNlIiwic29wLWNvbnRlbnQtc2VydmljZSIsInNvcC1wdXNoLXNldHRpbmctc2VydmljZSIsInNvcC1hcHAtc2VydmljZSIsInNvcC1wbGF0Zm9ybSIsInNvcF9zbXMiLCJzb3BfbWFpbCJdLCJhdHRyaWJ1dGVzIjp7ImxvZ2luVHlwZSI6InVzZXJuYW1lIiwibmlja05hbWUiOiJ1c2VyXzEwMDAwMDAwMjA0In0sImV4cCI6MTUxMDEzMzE1MiwianRpIjoiZDNjNzEyOGMtYTI5Zi00ZmZjLWI4YmItYTJhYWQwMDMxMDlhIiwiY2xpZW50X2lkIjoic29wX2FwcF9wbGF0Zm9ybSJ9.EBZcaEvcC37_fOn7mmmjhIvm2m_e2zbYyZ8648WcuB2_wzYWC_NEWkADS6mDX3q0Qw9ivcL14A0s5VbXKLEZMtpWUg1MMXDmLLrGUKQMjRhyyztvKpJSkKMNmq0Uj5sP8EdAiTGvHNi8QQrDEjIRsFHL_9UCPIR4I_mmw0MttmRjOjWrkYtTJgJfVGVBSjwhDO_0kWSrZ_hWjhKlSFHsYlt-CI1WKo-j5wwHiOLIB6AfXlbkfszq7gjlVA2qyrAUaxgCn8bvojVaYQpFqhwWQVBriPj2PNcDL68-QKd0uSgmwB_Akr3-xXei_f9ynhnYbJun8uSZMdXlavhr-A0F-w',
             },
